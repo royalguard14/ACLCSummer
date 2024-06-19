57c3a5c8541b4316b90a0b0badf6f60342497450
Ghaizar <ghaizar.bautista@aclcbutuan.edu.ph>
2024-06-19 09:14:18 +0800

    day2

--- Day2/DC/activity1.py
+++ Day2/DC/activity1.py
@@ -2,7 +2,29 @@ import os
 os.system("cls")
 
 
-num = 10
-print("This is double qoute")
-print('This is single qoute')
-print(type(25))
+print('single quotes = ','spam eggs') 
+print("double quotes = ","Paris rabbit got your back :)! Yay!" )
+print("digits and numerals enclosed in quotes are also strings = ",'1975')
+
+print('doesn\'t') # use \' to escape the single quote...
+print("doesn't") # ...or use double quotes instead
+
+print('"Yes," they said.')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said.')
+
+s = 'First line.\nSecond line.'
+print(s)
+
+print('C:\some\name') 
+print(r'C:\some\name')
+
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+
+
+'1975'  
