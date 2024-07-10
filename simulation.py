4dbc9a2128dac8a6d3dbe3c7a83039b034934c7a
royalguard14 <ghaizar.bautista@aclcbutuan.edu.ph>
2024-07-10 11:15:47 +0800

    Day 12

---
+++ simulation.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+letters = ['d', 's', 'm', 'r', 'a', 'e']
+for i in range(len(letters)):
+    print(letters[i])
+    #for j in range(0, len(letters)-i-1):
+        #print(letters[i],"==",letters[j])
